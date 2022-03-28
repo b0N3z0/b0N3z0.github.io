3ec75f7a9435f0371e352ed0c1f4d2a8f9b83712
b0N3z0 <bogdan11zakh@mail.ru>
2022-03-28 19:29:53 +0700

    Update main_page.php

--- components/main_page.php
+++ components/main_page.php
@@ -2,7 +2,7 @@
   <section>
     <h1>Welcome To The Queen Fanmade Website!</h1>
     <p>Our mission is to preserve and popularise the great heritage of Queen band and its magnificent performers.
-      <br> We are inspired by magnificent, but nonetheless tragic fate of the band's former member, beloved by many different people around the world - Freddie Mercury, and we want to share this grand legacy with others</p>
+      <br> We are inspired by gorgeous, but nonetheless tragic fate of the band's former member, beloved by many different people around the world - Freddie Mercury, and we want to share this grand legacy with others</p>
   </section>
 
   <section>
